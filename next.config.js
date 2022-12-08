3df989b9f87bed7db266497218eab32f8e4e6a1a
Md. Noyon Islam <md.nahidhasan@Mds-MacBook-Pro.local>
2022-12-08 22:26:40 +0600

    update confige

--- next.config.js
+++ next.config.js
@@ -1,15 +1,13 @@
-module.exports = async (phase, { defaultConfig }) => {
-  /**
-   * @type {import('next').NextConfig}
-   */
-  const nextConfig = {
-    reactStrictMode: true,
-    swcMinify: true,
-    /* config options here */
-    images: {
-      domains: ["v5.airtableusercontent.com"],
-    },
-  };
-
-  return nextConfig;
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  appDir: true,
+  images: {
+    domains: ["v5.airtableusercontent.com"],
+  },
 };
+
+module.exports = nextConfig;
